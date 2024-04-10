0b6d9d66648daaafe77084551bdfcdd9ba96b5da
Jheyson Guzman <jheysonguzman4817@gmail.com>
2024-04-09 20:00:34 -0500

    arreglo de dos dimensiones

--- Taller_4.py
+++ Taller_4.py
@@ -1,26 +1,10 @@
-#Importar la biblioteca
-import numpy as np
+#Crear arreglos de dos dimensiones a partir de listas
 
-#un metodo de crear arreglos ndarray es usando una lista normalita
+miLista3 = [(5,8,7,62), (3,2,5,1)]
 
-miLista = [3,5,7,9]
+miArreglo3 = np.array(miLista3)
+print (miArreglo3)
+print (miArreglo3.ndim)
+print (miArreglo3.shape)
+print (miArreglo3.size)
 
-miArreglo = np.array(miLista)
-
-#Dimensiones eje
-print ("Dimensiones:", miArreglo.ndim)
-
-#Dimensiones eje y longitud
-print ("Eje y Longitud:", miArreglo.shape)
-
-#Elementos
-print ("Elementos:", miArreglo.size)
-
-#Tipos de elementos
-print ("Tipos de elementos:", miArreglo.dtype)
-
-
-
-miArreglo2 = np.array([3,5,8,14])
-
-print (miArreglo2)

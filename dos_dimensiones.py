5f0eaf927647b6c403be9baeb43b27f8da8dd42f
Jheyson Guzman <jheysonguzman4817@gmail.com>
2024-04-09 20:13:38 -0500

    Numpy

---
+++ dos_dimensiones.py
@@ -0,0 +1,10 @@
+#Crear arreglos de dos dimensiones a partir de listas
+import numpy as np
+
+miLista = [(5,8,7,62), (3,2,5,1)]
+
+miArreglo = np.array(miLista)
+print (miArreglo)
+print (miArreglo.ndim)
+print (miArreglo.shape)
+print (miArreglo.size)
---
+++ relleno.py
@@ -0,0 +1,18 @@
+# Usando funciones de "relleno" de arreglos
+import numpy as np
+
+miArreglo = np.zeros ((4,5))
+
+print (miArreglo)
+
+
+miArreglo = np.ones ((4,5))
+
+print (miArreglo)
+
+miArreglo = np.empty ((2,2))
+
+print (miArreglo)
+
+
+print (miArreglo.dtype)

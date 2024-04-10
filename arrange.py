30907c3e55c4f0bfe5b8c45d7013580306d7aa45
Jheyson Guzman <jheysonguzman4817@gmail.com>
2024-04-09 20:46:11 -0500

    final

--- arrange.py
+++ arrange.py
@@ -4,6 +4,8 @@ import numpy as np
 # Genera un ciclo de numeros
 miArreglo = np.linspace(0,100,10)
 
-miArreglo = np.reshape((0,100,1000))
 
-print (miArreglo.size)
+miArreglo= miArreglo.reshape(2,5)
+
+print (miArreglo)
+print(miArreglo.size)
